08744d4c9c3aec3fb13e23d7f97a95ab06dc474f
blackjack <kurawata414@gmail.com>
2024-11-09 02:35:01 +0000

    ♻️ テストコードを分割

--- contents/src/ruby-book/lib/fizz_buzz.rb
+++ contents/src/ruby-book/lib/fizz_buzz.rb
@@ -16,20 +16,3 @@ def fizz_buzz(n)
         n.to_s
     end
 end
-
-# ここで、テストの自動実行を指示する
-# Minitest 自体は Ruby の標準ライブラリに含まれているため、
-# この require で import しているわけではない
-require 'minitest/autorun'
-
-class FizzBuzzTest < Minitest::Test
-    def test_fizz_buzz
-        assert_equal '1', fizz_buzz(1)
-        assert_equal '2', fizz_buzz(2)
-        assert_equal 'Fizz', fizz_buzz(3)
-        assert_equal '4', fizz_buzz(4)
-        assert_equal 'Buzz', fizz_buzz(5)
-        assert_equal 'Fizz', fizz_buzz(6)
-        assert_equal 'Fizz Buzz', fizz_buzz(15)
-    end
-end
